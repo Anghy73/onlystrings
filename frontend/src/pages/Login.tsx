34629f2a5593fff7b586eb85eaaf0cfa6c481ec1
Andy <anghy.ruz@gmail.com>
2025-06-03 18:47:09 -0400

    styles for login page

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -3,6 +3,8 @@ import { useForm } from "react-hook-form";
 import { Link, useNavigate } from "react-router";
 import { userLoginSchema } from "../schemas/zodSchemas";
 import { useTokenStore } from "../store/useTokenStore";
+import Back from "../components/Back";
+import { useState } from "react";
 
 interface UserLogin {
   email: string
@@ -10,6 +12,7 @@ interface UserLogin {
 }
 
 function Login() {
+  const [passwordError, setPasswordError] = useState<null | string>(null)
   const navigate = useNavigate()
   const setUser = useTokenStore(state => state.setUser)
   const { register, handleSubmit, formState: { errors } } = useForm<UserLogin>({
@@ -31,7 +34,7 @@ function Login() {
       console.log(json);
       if (json.error) {
         console.log(json.error);
-
+        setPasswordError(json.error)
       } else {
         navigate("/")
         setUser(json.user)
@@ -44,24 +47,35 @@ function Login() {
   })
 
   return (
-    <div className="flex flex-col gap-10 justify-center items-center w-screen h-screen px-20">
-      <div className="flex flex-col items-center gap-3">
-        <h3 className="font-black text-4xl">OnlyStrings Login</h3>
-        <p className="text-lg text-gray-400">You don't have a account? <span className="text-cyan-700 font-medium hover:text-cyan-600"><Link to="/register">let's create an account</Link></span></p>
+    <div className="relative overflow-hidden flex flex-col gap-10 justify-center items-center w-screen h-screen bg-[#1e1e20aa]">
+      {/* bg */}
+      <div className="absolute -top-20 -right-5 bg-[#ffffffbb] w-80 h-80 rounded-full opacity-40 shadow-[0px_0px_100px_50px_rgba(255,_255,_255,_1)]" />
+      <div className="absolute bottom-0 -left-20 bg-[#ffffffbb] w-80 h-80 rounded-full opacity-40 shadow-[0px_0px_100px_50px_rgba(255,_255,_255,_1)]" />
+      <Back />
+
+      <div className="flex flex-col justify-center items-center w-full h-full backdrop-blur-2xl">
+        <div className="flex flex-col items-center gap-3">
+          <h3 className="font-black text-4xl">OnlyStrings Login</h3>
+          <p className="text-lg text-gray-400">You don't have a account? <span className="text-cyan-700 font-medium hover:text-cyan-600"><Link to="/register">let's create an account</Link></span></p>
+        </div>
+        <form onSubmit={onSubmit} className="flex flex-col gap-4 w-full max-w-2xl mt-20">
+          <div className="flex flex-col">
+            {/* <label className="text-lg mb-2 font-semibold">Email</label> */}
+            <input className="p-3 rounded-2xl border-2 border-gray-700/70 focus-visible:border-gray-600 bg-[#1e1e20]/70 outline-0" type="email" {...register("email")} placeholder="Email" />
+            <span className="text-red-400">{errors.email?.message}</span>
+          </div>
+          <div className="flex flex-col">
+            {/* <label className="text-lg mb-2 font-semibold">Password</label> */}
+            <input className="p-3 rounded-2xl border-2 border-gray-700/70 focus-visible:border-gray-600 bg-[#1e1e20]/70 outline-0" type="password" {...register("password")} placeholder="Password" />
+            <span className="text-red-400">{errors.password?.message}</span>
+            {
+              passwordError && <p>{passwordError}</p>
+            }
+          </div>
+          <button className="bg-cyan-950 p-3 rounded-md cursor-pointer font-semibold text-xl hover:bg-cyan-900 mt-4" type="submit">Enter</button>
+        </form>
       </div>
-      <form onSubmit={onSubmit} className="flex flex-col gap-4 w-full max-w-2xl">
-        <div className="flex flex-col">
-          <label className="text-lg mb-2 font-semibold">Email</label>
-          <input className="p-2 rounded-lg border-2 border-cyan-950 focus-visible:border-cyan-600 outline-0" type="email" {...register("email")} />
-          <span className="text-red-400">{errors.email?.message}</span>
-        </div>
-        <div className="flex flex-col">
-          <label className="text-lg mb-2 font-semibold">Password</label>
-          <input className="p-2 rounded-lg border-2 border-cyan-950 focus-visible:border-cyan-600 outline-0" type="password" {...register("password")} />
-          <span className="text-red-400">{errors.password?.message}</span>
-        </div>
-        <button className="bg-cyan-950 p-3 rounded-md cursor-pointer font-semibold text-xl hover:bg-cyan-900 mt-4" type="submit">Enter</button>
-      </form>
+
     </div>
   )
 }
